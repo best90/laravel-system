4f32bde9ca30d411ec233bda146f853c2859825c
Carlo J <422247668@qq.com>
2017-04-02 08:03:36 +0800

    [add] add database tables config

---
+++ app/Http/Tables.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http;
+
+class Tables
+{
+    //数据库表
+    const TABLES = [
+        'Admin' => 'admin',
+        'Bp' => 'bp',
+        'Enroll' => 'enroll',
+        'EnrollAttachment' => 'enroll_attachment',
+        'EnrollPageItemValue' => 'enrollpage_item_value',
+        'EnrollPageTemplateItem' => 'enrollpage_template_item',
+        'Industry' => 'industry',
+        'IndustryRaceZone' => 'industry_race_zone',
+        'Judge' => 'judge',
+        'LogSendBpReview' => 'log_send_bp_review',
+        'RaceAffair' => 'race_affair',
+        'RaceAffairJudgeMap' => 'race_affair_judge_map',
+        'RacePublicitySetting' => 'race_publicity_setting',
+        'RaceUnit' => 'race_unit',
+        'RaceUnitBpJudgeMap' => 'race_unit_bp_judge_map',
+        'RaceUnitBpMap' => 'race_unit_bp_map',
+        'RaceUnitBpReview' => 'race_unit_bp_review',
+        'RaceUnitBpScoreData' => 'race_unit_bp_score_data',
+        'RaceZone' => 'race_zone',
+        'RaceZoneAdminMap' => 'race_zone_admin_map',
+        'ScoreStandard' => 'score_standard'
+    ];
+}
