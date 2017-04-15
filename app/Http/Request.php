46d971a62906b426f815002ce1bda67a46373239
Carlo J <422247668@qq.com>
2017-04-15 19:04:05 +0800

    [add] add request class

---
+++ app/Http/Request.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017-04-10
+ * Time: 18:13
+ */
+
+namespace App\Http;
+
+class Request
+{
+    public $host;
+    public $header;
+
+    public function __construct(string $host,array $header = [])
+    {
+        $this->host = $host ?? 'http://127.0.0.1/';
+        $this->header = $header ?? ['Cache-Control:no-cache'];
+    }
+
+    /**
+     * @param string $path
+     * @param array $data
+     * @return mixed
+     */
+    public function post(string $path = '', array $data = [])
+    {
+        $url = $this->host.$path;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        $api_data = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($api_data,true);
+    }
+
+    /**
+     * @param string $path
+     * @return mixed
+     */
+    public function get(string $path = '')
+    {
+        $url = $this->host.$path;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $api_data = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($api_data,true);
+    }
+}
